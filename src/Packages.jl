605fcff331a0a78902a122df8c4e541f4c7e1ce9
Mathilde Jeannin <jeannin@lix.polytechnique.fr>
2025-11-19 16:33:56 +0100

    update fichier install

--- src/Packages.jl
+++ src/Packages.jl
@@ -1,15 +1,13 @@
 using Observables
-using GLMakie
-using GraphMakie
+#using GLMakie
+#using GraphMakie
 using Graphs
 using InteractiveDynamics
-using DataStructures: CircularBuffer
 using Random
 using Makie.Colors
 using Agents
 using POMDPs
 using MCTS
-using Random
 using D3Trees
 using StaticArrays
 using POMDPTools: Deterministic, Uniform, SparseCat, ImplicitDistribution, RandomSolver
@@ -17,10 +15,8 @@ using POMDPModels
 using StructEquality
 using Dates
 using DelimitedFiles
-using Observables
 using Base.Threads
 using CSV
 using DataFrames
-using Dates
 using DataStructures
 using AStarSearch
